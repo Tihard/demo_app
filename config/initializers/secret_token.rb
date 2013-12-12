8df8066a9f12e6644800a76947f79442f6a8a08a
Tiago Fernandes <tiago.fffernandes@hotmail.com>
2013-12-12 01:04:38 -0200

    scaffold user and micropost, db migration

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = 'abaed2829406c2d477444b6686e70cb0fe70b13c906fb1672eef170f3428e6bd0b69fd85cf9907d56cb20c8112ad9b92e229e148dff2eeec9cf2c61ada6fe369'
